0fc76815c3b7084cb2384445ee49afe455a2493c
raj padekar <yashraj.kuyate24@mmit.edu.in>
2026-06-11 00:44:17 +0530

    Fix vitest executing playwright tests by adding exclude path

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     globals: true,
+    exclude: ['e2e/**', 'node_modules/**', '.next/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
